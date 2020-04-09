04f439627ae121de43f41d51524dc07e07e5a37c
mamulini <mamulini@yahoo.com>
2020-04-09 15:34:42 +0300

    initial commit

---
+++ .history/js/script_20200409141133.js
@@ -0,0 +1,12 @@
+document.getElementById('themeSwitch').addEventListener('change', event => {
+    let themeTitle = document.querySelector('.theme__title');
+    if (event.target.checked) {
+        document.body.setAttribute('data-theme', 'light');
+        themeTitle.textContent = 'Dark mode';
+    } else {
+        document.body.removeAttribute('data-theme');
+        themeTitle.textContent = 'Dark mode';
+    }
+});
+
+// (event.target.checked) ? document.body.setAttribute('data-theme', 'light') : document.body.removeAttribute('data-theme');
